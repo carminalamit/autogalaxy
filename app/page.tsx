6a77c55246edc70fcec9a5cd4c6466dd483594b6
minalamit <lamitcarmina@gmail.com>
2023-07-31 17:29:31 +0800

    started to add search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import { Hero } from "@/components"
+import Image from "next/image"
+import { fuels, yearsOfProduction } from "@/constants"
+import { SearchBar, CustomFilter, Hero } from "@/components";
 
 export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+
+      </div>
     </main>
   )
 }
