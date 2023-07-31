74f4abb2e900d119471131071fedd61616473173
minalamit <lamitcarmina@gmail.com>
2023-07-31 22:38:53 +0800

    edited

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,8 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
-import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
 
 export {
     Hero,
@@ -13,5 +14,6 @@ export {
     Footer,
     SearchBar,
     CustomFilter,
-    SearchManufacturer
+    CarCard,
+    ShowMore
 }
